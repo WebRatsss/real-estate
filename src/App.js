9c7f037383b5a2f7958f976ba9173ac5802b6cf8
ansh-srivastava <ansh.srivastava0987@gmail.com>
2024-03-19 20:12:41 +0530

    routing completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  // {
+  //   path: "/hero",
+  //   element: <Hero/>,
+  // },
+  // {
+  //   path: "/Hero",
+  //   element: <Hero/>,
+  // },
+  // {
+  //   path: "/contact",
+  //   element: <Contact/>,
+  // },
+  // {
+  //   path: "/gallery",
+  //   element: <Gallery/>,
+  // },
+  // {
+  //   path: "/banner",
+  //   element: <Banner/>,
+  // },
+  // {
+  //   path: "/about",
+  //   element: <About/>,
+  // },
+  // {
+  //   path: "/portfolio",
+  //   element: <Portfolio/>,
+  // }
+]);
 
 function App() {
   return (
-    <div className='text-red-400'>Hello World</div>
+    <RouterProvider router={router} />
   );
 }
 
 export default App;
+
